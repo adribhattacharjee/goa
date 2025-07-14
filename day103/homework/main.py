53374aca90380cb40d0ff8c0449dacb809a45b8a
adribhattacharjee <163740212+adribhattacharjee@users.noreply.github.com>
2025-07-14 16:06:04 +0300

    1

--- day103/homework/main.py
+++ day103/homework/main.py
@@ -13,7 +13,12 @@
 # print(func("123"))
 # 2) შექმენი ფუნქცია, რომელიც პარამეტრად მიიღებს რიცხვს და დაბეჭდავს მას შემობრუნებულს.
 # მაგალითად: 12345 -> 54321
+def reversed_number(number):
+    reversed_number = str(number)[::-1]
 
+    print(reversed_number)
+
+reversed_number(12345) 
 
 # 3) შექმენი ფუნქცია, რომელიც მომხმარებელს შემოატანინებს რიცხვს და დაბეჭდავს 1-დან შემოტანილ რიცხვამდე ყველა რიცხვის კვადრატის ჯამს.
 def cubes():
@@ -21,15 +26,14 @@ def cubes():
     cube = int(input("please enter a number"))
     for i in range(1,cube):
         num1 += i*i
-    print(num1)
+        print(num1)
 cubes()
-# 4) შექმენი ფუნქცია, რომელიც პარამეტრად მიიღებს 2 რიცხვს. ამ ფუნქციამ უნდა დააბრუნოს პირველი მიღებული რიცხვი მეორე მიღებული რიცხვის ხარისხში
 
 # 5) შექმენი ფუნქცია, რომელიც მიიღებს 3 რიცხვს. გაიგებს მათ შორის მაქსიმალურს და მინიმალურს. შემდეგ კი დააბრუნებს მაქსიმალური და მინიმალური რიცხვის ნამრავლს. 
-# def product_of_min_and_max(a, b, c):
-#     minimum = min(a, b, c)
-#     maximum = max(a, b, c)
-#     return minimum , maximum
+def product_of_min_and_max(a, b, c):
+     minimum = min(a, b, c)
+     maximum = max(a, b, c)
+     return minimum , maximum
 
-# result = product_of_min_and_max(3, 7, 5)
-# print(result)
+result = product_of_min_and_max(3, 7, 5)
+print(result)
